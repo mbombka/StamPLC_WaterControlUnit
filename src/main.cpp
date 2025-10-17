44fd875417f8303f872ea270533957ec52699b75
babkma1 <mariusz.babka@skan.com>
2025-10-17 16:36:48 +0200

    working DS1820

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,22 @@
  */
 #include <Arduino.h>
 #include <M5StamPLC.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#include <screenHelper.cpp>
+
+ #define ONE_WIRE_BUS 4  //DS18B20 on Grove B corresponds to GPIO26 on ESP32
+
+  OneWire oneWire(ONE_WIRE_BUS);
+  DallasTemperature DS18B20(&oneWire);
+
+/*  Resolution of DS18B20 temperatur measurement = power-on-default = 12 bit = 0.0625 C
+   *9-bit resolution --> 93.75 ms
+   *10-bit resolution --> 187.5 ms
+   *11-bit resolution --> 375 ms
+   *12-bit resolution --> 750 ms
+*/ 
+  String monitorstring = "";  
 
 void setup()
 {
@@ -12,48 +28,54 @@ void setup()
     M5StamPLC.begin();
 }
 
+
 void loop()
-{
-    /* Fill screen with colors */
-    M5StamPLC.Display.fillScreen(TFT_WHITE);
-    delay(800);
-    M5StamPLC.Display.fillScreen(TFT_RED);
-    delay(800);
-    M5StamPLC.Display.fillScreen(TFT_GREEN);
-    delay(800);
-    M5StamPLC.Display.fillScreen(TFT_BLUE);
-    delay(800);
-    M5StamPLC.Display.fillScreen(TFT_BLACK);
-    delay(800);
+{ 
+     float celsius;
+    float fahrenheit;
 
-    /* Set cursor and print text */
-    M5StamPLC.Display.setCursor(10, 10);
-    M5StamPLC.Display.setTextColor(TFT_WHITE);
-    M5StamPLC.Display.setTextSize(1);
-    M5StamPLC.Display.printf("Display Test!");
-    delay(800);
+    monitorstring = "Date;Time";  // change to realtime date and time of M5Stack (later)
+    
+    DS18B20.begin();
+    int count = DS18B20.getDS18Count();  //check for number of connected sensors
 
-    /* Draw shapes */
-    M5StamPLC.Display.drawRect(100, 50, 50, 50, BLUE);
-    delay(800);
-    M5StamPLC.Display.fillRect(100, 50, 50, 50, BLUE);
-    delay(800);
-    M5StamPLC.Display.drawCircle(100, 50, 50, RED);
-    delay(800);
-    M5StamPLC.Display.fillCircle(100, 50, 50, RED);
-    delay(800);
-    M5StamPLC.Display.drawTriangle(30, 30, 180, 50, 80, 100, YELLOW);
-    delay(800);
-    M5StamPLC.Display.fillTriangle(30, 30, 180, 50, 80, 100, YELLOW);
-    delay(800);
-    delay(800);
+//    M5.Lcd.clear();    //clearing causes flickering of Lcd-display, looks nicer without
+    M5.Lcd.setCursor(0,0);
+    M5.Lcd.print("Devices found: ");
+    M5.Lcd.print(count);
+    M5.Lcd.println(" DS18B20");
 
-    /* Draw random triangles */
-    for (int i = 0; i < 1000; i++) {
-        M5StamPLC.Display.fillTriangle(random(M5StamPLC.Display.width() - 1), random(M5StamPLC.Display.height() - 1),
-                                       random(M5StamPLC.Display.width() - 1), random(M5StamPLC.Display.height() - 1),
-                                       random(M5StamPLC.Display.width() - 1), random(M5StamPLC.Display.height() - 1),
-                                       random(0xfffe));
+    if (count > 0) {
+    DS18B20.requestTemperatures();
+    for (int i=0; i < count; i++) {
+      String m5stackstring = "Sensor ";
+ 
+      celsius = DS18B20.getTempCByIndex(i);
+
+      m5stackstring = m5stackstring + String(i) + String(": ") + String(celsius,4) + String(" C     ");  
+      M5.Lcd.println(m5stackstring);  // 1 line per sensor on M5Stack Lcd
+    
+      monitorstring = monitorstring + String(";") + String(celsius,4);  // ";" is Excel compatibel separator
+ 
     }
-    delay(800);
+    Serial.println(monitorstring);  // 1 line for all measurements on serial monitor
+    }
+    delay(500); //Measuremnt interval
+//      /* Show menu */
+//     showMenu();
+
+    
+//    /* Check if button was clicked */
+//     if (M5.BtnA.wasClicked()) {
+//         showHeatingStarted();
+//     } else if (M5.BtnB.wasClicked()) {
+//         showHeatingStopped();;
+//     } else if (M5.BtnC.wasClicked()) {
+      
+//             //showSensorStatus(45.5, 38.2, true, false, true, false);
+//     }
+
+//     delay(100);
+
 } 
+
