fedcceff1f0594e57264d193ab12dd63b40238bd
mbombka <mbombka@gmail.com>
2025-10-22 21:55:39 +0200

    added logic for sensors

--- src/main.cpp
+++ src/main.cpp
@@ -11,8 +11,14 @@
 
  #define ONE_WIRE_BUS 4  //DS18B20 on Grove B corresponds to GPIO26 on ESP32
 
-  OneWire oneWire(ONE_WIRE_BUS);
-  DallasTemperature DS18B20(&oneWire);
+//parameters for the water
+const float MAXIMUM_WATER_TEMP = 80;
+const float HYSTERESIS = 8;
+const float NORMAL_WATER_TEMPERATURE = 40;
+const float BATH_WATER_TEMPERATURE = 60;
+
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature DS18B20(&oneWire);
 
 /*  Resolution of DS18B20 temperatur measurement = power-on-default = 12 bit = 0.0625 C
    *9-bit resolution --> 93.75 ms
@@ -20,7 +26,24 @@
    *11-bit resolution --> 375 ms
    *12-bit resolution --> 750 ms
 */ 
-  String monitorstring = "";  
+String monitorstring = "";  
+//enumarator for scren choose
+enum screen{
+    MAIN_SCREEN,
+    OK_SCREEN,
+    STOP_SCREEN,
+    SENSORS_SCREEN
+};
+//enumerator for mode
+enum mode{
+    OFF,
+    CIRCULATION,
+    BATH
+};
+
+screen actualScreen = MAIN_SCREEN;
+mode acualMode = OFF;
+void contolLogic(float temperature1, float temperature2, bool& pumpHotWaterIsOn, bool& pumpCirculationIsOn, bool& pumpFloorHeatingIsOn);
 
 void setup()
 {
@@ -28,54 +51,87 @@ void setup()
     M5StamPLC.begin();
 }
 
-
 void loop()
 { 
-     float celsius;
-    float fahrenheit;
-
+    float celsius, temperature1, temperature2;
+    bool pumpHotWaterIsOn, pumpCirculationIsOn, pumpFloorHeatingIsOn;
     monitorstring = "Date;Time";  // change to realtime date and time of M5Stack (later)
     
     DS18B20.begin();
     int count = DS18B20.getDS18Count();  //check for number of connected sensors
+    //read status of relays 
+    pumpHotWaterIsOn =  M5StamPLC.readPlcRelay(0);
+    pumpCirculationIsOn =  M5StamPLC.readPlcRelay(1);
+    pumpFloorHeatingIsOn =  M5StamPLC.readPlcRelay(2);
 
-//    M5.Lcd.clear();    //clearing causes flickering of Lcd-display, looks nicer without
-    M5.Lcd.setCursor(0,0);
-    M5.Lcd.print("Devices found: ");
-    M5.Lcd.print(count);
-    M5.Lcd.println(" DS18B20");
-
-    if (count > 0) {
-    DS18B20.requestTemperatures();
-    for (int i=0; i < count; i++) {
-      String m5stackstring = "Sensor ";
- 
-      celsius = DS18B20.getTempCByIndex(i);
-
-      m5stackstring = m5stackstring + String(i) + String(": ") + String(celsius,4) + String(" C     ");  
-      M5.Lcd.println(m5stackstring);  // 1 line per sensor on M5Stack Lcd
-    
-      monitorstring = monitorstring + String(";") + String(celsius,4);  // ";" is Excel compatibel separator
- 
+    //read temperature
+    if (count > 2) {
+        DS18B20.requestTemperatures();
+        temperature1 = DS18B20.getTempCByIndex(0);
+        temperature2 = DS18B20.getTempCByIndex(1);
+        monitorstring = "Sensor0: " + String(temperature1,1) + ", Sensor1: " + String(temperature2,1); 
+    }
+    else {
+        monitorstring = "Problem, number of found sensors: " + String(count);
     }
     Serial.println(monitorstring);  // 1 line for all measurements on serial monitor
+
+   /* Check if button was clicked */
+    if (M5.BtnA.wasClicked()) {
+        acualMode = BATH;
+        actualScreen = OK_SCREEN;
+    } else if (M5.BtnB.wasClicked()) {
+        acualMode = OFF;
+        actualScreen = STOP_SCREEN;
+    } else if (M5.BtnC.wasClicked()) {
+        actualScreen = SENSORS_SCREEN;           
     }
-    delay(500); //Measuremnt interval
-//      /* Show menu */
-//     showMenu();
+    //actual logic
+    contolLogic(temperature1, temperature2, pumpHotWaterIsOn, pumpCirculationIsOn, pumpFloorHeatingIsOn);
 
+    //display screen
+    switch (actualScreen)
+    {
+    case MAIN_SCREEN:
+        showMenu();
+        break;
+    case OK_SCREEN:
+        showHeatingStarted();
+        break;
+    case STOP_SCREEN:
+        showHeatingStopped();
+        break;
+    case SENSORS_SCREEN:
+        showSensorStatus(temperature1, temperature2, pumpHotWaterIsOn, pumpCirculationIsOn, pumpFloorHeatingIsOn);
+        break;
+    default:
+        break;
+    }
     
-//    /* Check if button was clicked */
-//     if (M5.BtnA.wasClicked()) {
-//         showHeatingStarted();
-//     } else if (M5.BtnB.wasClicked()) {
-//         showHeatingStopped();;
-//     } else if (M5.BtnC.wasClicked()) {
-      
-//             //showSensorStatus(45.5, 38.2, true, false, true, false);
-//     }
-
-//     delay(100);
+    //Write relays
+    M5StamPLC.writePlcRelay(0, pumpHotWaterIsOn);
+    M5StamPLC.writePlcRelay(1, pumpCirculationIsOn);
+    M5StamPLC.writePlcRelay(2, pumpFloorHeatingIsOn);    
+    
+    delay(500); //Measuremnt interval
 
 } 
 
+void contolLogic(float temperature1, float temperature2, bool& pumpHotWaterIsOn, bool& pumpCirculationIsOn, bool& pumpFloorHeatingIsOn){
+//logic
+     switch (acualMode)
+    {
+    case OFF:
+
+        break;
+    case CIRCULATION:
+        break;
+    case BATH:
+        break;
+    default:
+        break;
+    }
+    
+
+    }
+};
--- src/screenHelper.cpp
+++ src/screenHelper.cpp
@@ -11,7 +11,7 @@ static void showMenu(){
     delay(100);
 }
 
-static void showSensorStatus(float temperature1, float temperature2, bool pumpHotWaterIsOn, bool pumpCirculationIsOn, bool pumpHeating1IsOn, bool pumpHeating2IsOn){
+static void showSensorStatus(float temperature1, float temperature2, bool pumpHotWaterIsOn, bool pumpCirculationIsOn, bool pumpFloorHeatingIsOn){
     M5StamPLC.Display.clear(TFT_BLACK);
     M5StamPLC.Display.setCursor(0, 10);
     M5StamPLC.Display.setTextColor(TFT_WHITE);
@@ -20,8 +20,7 @@ static void showSensorStatus(float temperature1, float temperature2, bool pumpHo
     M5StamPLC.Display.printf(" T. sprzegla:%.2fC\n", temperature2);
     M5StamPLC.Display.printf(" Pompa CWU: %s\n", pumpHotWaterIsOn? "ON":"OFF");
     M5StamPLC.Display.printf(" Pompa cyrk.: %s\n", pumpCirculationIsOn? "ON":"OFF");
-    M5StamPLC.Display.printf(" Pompa parteru: %s\n", pumpHeating1IsOn? "ON":"OFF");
-    M5StamPLC.Display.printf(" Pompa pietra: %s\n", pumpHeating2IsOn? "ON":"OFF");
+    M5StamPLC.Display.printf(" Pompa parteru: %s\n", pumpFloorHeatingIsOn? "ON":"OFF");
     delay(5000);
 }
 
