c063809a8a68eb1be7ab66a58763f93b481abec1
babkma1 <mariusz.babka@skan.com>
2025-10-27 12:33:52 +0100

    wroking menu and bath mode,.

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,3 @@
-/*
- * SPDX-FileCopyrightText: 2025 M5Stack Technology CO LTD
- *
- * SPDX-License-Identifier: MIT
- */
 #include <Arduino.h>
 #include <M5StamPLC.h>
 #include <OneWire.h>
@@ -14,20 +9,12 @@
 
 using namespace std::chrono;
 
- #define ONE_WIRE_BUS 4  //DS18B20 on Grove B corresponds to GPIO26 on ESP32
-
-
- //structure for time of day
- struct hourMinute
- {
-   int hour;
-   int minute;
- };
+#define ONE_WIRE_BUS 4  //DS18B20 on Grove B corresponds to GPIO26 on ESP32
  
 //parameters for the water
-const float NORMAL_HOT_WATER_TEMPERATURE = 50;
+const float NORMAL_HOT_WATER_TEMPERATURE = 25;
 const float FLOOR_HEATING_TEMPERATURE = 35;
-const float BATH_WATER_TEMPERATURE = 65;
+const float BATH_WATER_TEMPERATURE = 27;
 const float MAXIMUM_WATER_TEMP = 80;
 const float HYSTERESIS = 4;
 const int PRE_BATH_CIRC_TIME = 3;
@@ -45,27 +32,20 @@ DFRobot_GP8XXX_IIC GP8413(RESOLUTION_15_BIT, 0x59, &Wire); //DAC output
 OneWire oneWire(ONE_WIRE_BUS); 
 DallasTemperature DS18B20(&oneWire);
 
-/*  Resolution of DS18B20 temperatur measurement = power-on-default = 12 bit = 0.0625 C
-   *9-bit resolution --> 93.75 ms
-   *10-bit resolution --> 187.5 ms
-   *11-bit resolution --> 375 ms
-   *12-bit resolution --> 750 ms
-*/ 
 
-
-String monitorstring = "";  
 //enumerator for scren choose
 enum screen{
     MAIN_SCREEN,
-    OK_SCREEN,
-    STOP_SCREEN,
+    BATH_MODE_STARTED,
+    NORMAL_MODE_STARTED,
     SENSORS_SCREEN
 };
 //enumerator for mode
 enum mode{
-    NORMAL,
-    CIRCULATION,
-    BATH
+    NORMAL = 0,
+    BATH = 1,
+    CIRCULATION = 2
+    
 };
 /* relays and sensors: */
 float temperature1; //temperature of water in tank [C]
@@ -74,24 +54,27 @@ bool pumpHotWaterIsOn; // relay that turn on pump for heat water (it heat up wat
 bool pumpCirculationIsOn; //relay that turn on pump for hot water circulation( it circulate water in entire house). NO contact
 bool powerOffFloorHeatingPump; //relay that cut down power on pumps for floor heating. NC contact
 
-screen actualScreen = MAIN_SCREEN;
-mode acualMode = NORMAL;
+screen actualScreen = MAIN_SCREEN; //current displayed screen
+mode acualMode; //mode of operation
+mode previousMode; //previous mode of operation
+int bathStep = 0; //step in bath mode
+String monitorstring = "";  //string for serial monitor output
+String bathStepString = "";//string for bath step description
 unsigned long secondsFromButtonPressed; //numbe of secconds that passed from last pressing button
 void contolLogic(); // main method for controling relays
 void setTemperature(float temp); //helper function for setting temperature output
 void timeHelper(); // helper function for time handling
 bool risingEdge100ms, risingEdge500ms, risingEdge1s; //single signal that is high for 1 cycle cyclically
-bool pumpHotWaterIsOn, pumpCirculationIsOn, powerOffFloorHeatingPump, triggerCirculation, hotWaterHeatingActive, triggerBath, preBathCirculationDone, preBathCirculationTriggered, beforeBathCirculationDone;
+bool triggerCirculation, hotWaterHeatingActive;
 int memoCirculationDuration;
 
 float heaterSetTemperature, hotWaterTankSetTemperature, lastTemperatureSet;
 time_point<steady_clock> helperTime100ms, helperTime500ms, helperTime1s, buttonPressedTime, circtulationStartTime, bathTempStartTime, lastRelayChangeTime;//helper variables
 
 
-
 void setup()
 {
-    /* Init M5StamPLC */
+    // Init M5StamPLC
     M5StamPLC.begin();
     Serial.begin(9600);
 
@@ -101,6 +84,15 @@ void setup()
     helperTime1s = steady_clock::now();
 
     heaterSetTemperature = NORMAL_HOT_WATER_TEMPERATURE;
+
+    //start IE2C for DAC
+    Wire.end();
+    Wire.begin(2,1); //SDA, SCL, frequency
+
+    if (GP8413.begin() != 0) {
+        Serial.println("Init of DAC2 Fail!");
+        delay(1000);
+    }
 }
 
 void loop()
@@ -122,26 +114,29 @@ void loop()
         DS18B20.requestTemperatures();
         temperature1 = DS18B20.getTempCByIndex(0);
         temperature2 = DS18B20.getTempCByIndex(1);
-        monitorstring = "Sensor0: " + String(temperature1,1) + ", Sensor1: " + String(temperature2,1); 
+       // monitorstring = "Sensor0: " + String(temperature1,1) + ", Sensor1: " + String(temperature2,1); 
     }
-    else {
+    else if  (count < 2 && risingEdge1s)   {
         monitorstring = "Problem, number of found sensors: " + String(count);
     }
-    Serial.println(monitorstring);  // 1 line for all measurements on serial monitor
 
-   /* Check if button was clicked */
-    if (M5.BtnA.wasClicked()) {
-        acualMode = BATH;
-        bathStep = 0;
-        actualScreen = OK_SCREEN;
+   // Button handling
+    if (M5.BtnA.isPressed()) {         
+        actualScreen = BATH_MODE_STARTED;
         Serial.println("Button A pressed");
         buttonPressedTime = steady_clock::now();
-    } else if (M5.BtnB.wasClicked()) {
+        if(acualMode != BATH){
+          acualMode = BATH;
+          Serial.println("Mode set by button to " + String(acualMode));
+          bathStep = 0;
+        }       
+    } else if (M5.BtnB.isPressed()) {
         acualMode = NORMAL;
-        actualScreen = STOP_SCREEN;
+        actualScreen = NORMAL_MODE_STARTED;
+        Serial.println("Mode set by button to " + String(acualMode));
         Serial.println("Button B pressed");
         buttonPressedTime = steady_clock::now();
-    } else if (M5.BtnC.wasClicked()) {
+    } else if (M5.BtnC.isPressed()) {
         actualScreen = SENSORS_SCREEN;  
         Serial.println("Button C pressed");     
         buttonPressedTime = steady_clock::now();    
@@ -156,22 +151,23 @@ void loop()
     case MAIN_SCREEN:
         showMenu(risingEdge100ms);
         break;
-    case OK_SCREEN:
-         showHeatingStarted(risingEdge100ms);
-        if (secondsFromButtonPressed > 5) {
+    case BATH_MODE_STARTED:         
+        showHeatingWithText(risingEdge100ms, heaterSetTemperature, temperature1, bathStepString);  
+        if (acualMode == NORMAL) {
             actualScreen = MAIN_SCREEN;
+            Serial.println("Back to main screen"); 
         };
         break;
-    case STOP_SCREEN:
-        showHeatingStopped(risingEdge100ms);
+    case NORMAL_MODE_STARTED:
+        showNormalModeStarted(risingEdge100ms);
         if (secondsFromButtonPressed > 5) {
             actualScreen = MAIN_SCREEN;
         };
         break;
     case SENSORS_SCREEN:
-        showSensorStatus(temperature1, temperature2, pumpHotWaterIsOn, pumpCirculationIsOn, powerOffFloorHeatingPump, risingEdge100ms);
-        if (secondsFromButtonPressed > 5) {
-            actualScreen = MAIN_SCREEN;
+        showSensorStatus(heaterSetTemperature, temperature1, temperature2, pumpHotWaterIsOn, pumpCirculationIsOn, powerOffFloorHeatingPump, risingEdge100ms, static_cast<int>(acualMode), bathStep);
+        if (secondsFromButtonPressed > 10) {
+            actualScreen =  (acualMode == NORMAL) ? MAIN_SCREEN : BATH_MODE_STARTED;
         };
         break;
     default:
@@ -182,17 +178,15 @@ void loop()
     M5StamPLC.writePlcRelay(0, pumpHotWaterIsOn);
     M5StamPLC.writePlcRelay(1, pumpCirculationIsOn);
     M5StamPLC.writePlcRelay(2, powerOffFloorHeatingPump);    
-    
-    //update status on serial
-    Serial.println("Actual Screen:" + String(actualScreen));  
-    if (secondsFromButtonPressed > 0) {
-         Serial.println("Seconds from button press:" + String(secondsFromButtonPressed));  
+       
+    if(previousMode != acualMode){
+        Serial.println("Mode changed from " + String(previousMode) + " to " + String(acualMode));
+        previousMode = acualMode;
     }
-    Serial.println("Actual mode: " + String(acualMode)); 
-   
-    delay(500); //Measuremnt interval
-
 } 
+
+/* END OF LOOP*/
+
 
 //control logic for relays and temperature
 void contolLogic(){
@@ -207,7 +201,7 @@ void contolLogic(){
     //hot water tank logic should be independent
     if (temperature1 < hotWaterTankSetTemperature - HYSTERESIS) {
          hotWaterHeatingActive = true;
-    } elsif (temperature1 > hotWaterTankSetTemperature + HYSTERESIS){
+    } else if (temperature1 > hotWaterTankSetTemperature + HYSTERESIS){
         hotWaterHeatingActive = false;
     }
 
@@ -215,7 +209,7 @@ void contolLogic(){
     switch (acualMode)
     {
     case NORMAL: 
-
+        
          hotWaterTankSetTemperature = NORMAL_HOT_WATER_TEMPERATURE;
          
         //set normal temperature temperature
@@ -232,13 +226,15 @@ void contolLogic(){
             if (circulation == (actualHour, actualMinute)){
                 memoCirculationDuration = circulation.duration;
                 acualMode = CIRCULATION;
+                Serial.println("Mode set by circulation start to " + String(BATH));
                 triggerCirculation = true;
-                Serial.println("Circulation started");
+                Serial.println("Circulation started");  
+                circulation.print();
             }
         }        
         break;
 
-    case CIRCULATION:
+    case CIRCULATION: 
         //start only if water is heated
         if ((temperature1 > NORMAL_HOT_WATER_TEMPERATURE - HYSTERESIS) && (temperature2 > NORMAL_HOT_WATER_TEMPERATURE- HYSTERESIS) && triggerCirculation) {
             pumpCirculationIsOn = true;
@@ -249,11 +245,12 @@ void contolLogic(){
         if(duration_cast<minutes>(now - circtulationStartTime).count() >= memoCirculationDuration) {
             pumpCirculationIsOn = false;
             acualMode = NORMAL;
+            Serial.println("Mode set by circulation end to " + String(BATH));
             Serial.println("Circulation ended");
         }
         heaterSetTemperature = NORMAL_HOT_WATER_TEMPERATURE;
         hotWaterTankSetTemperature = NORMAL_HOT_WATER_TEMPERATURE;
-        setTemperature(heaterSetTemperature);
+        setTemperature(heaterSetTemperature); 
         break;
 
     case BATH:
@@ -265,13 +262,16 @@ void contolLogic(){
             heaterSetTemperature = NORMAL_HOT_WATER_TEMPERATURE;
             hotWaterTankSetTemperature = NORMAL_HOT_WATER_TEMPERATURE;
             setTemperature(heaterSetTemperature);
-            Serial.println("Bath mode started");
+            Serial.println("Bath mode started. BathStep " + String(bathStep));            
+            bathStepString = " Zadano temp cyrk.\n";
             bathStep ++;
-            break;
+            break; 
         case 1: //turn on circulation when temperature is reached
             if((temperature1 > NORMAL_HOT_WATER_TEMPERATURE - HYSTERESIS) && (temperature2 > NORMAL_HOT_WATER_TEMPERATURE- HYSTERESIS)){
-                umpCirculationIsOn = true;
+                pumpCirculationIsOn = true;
                 circtulationStartTime = steady_clock::now();
+                bathStepString =bathStepString + " Cyrk. w toku...\n";
+                Serial.println("BathStep " + String(bathStep)); 
                 bathStep ++;
             }
             break;
@@ -279,6 +279,8 @@ void contolLogic(){
         case 2: //wait for pre bath circulation done 
             if(duration_cast<minutes>(now - circtulationStartTime).count() >= PRE_BATH_CIRC_TIME) {
                 pumpCirculationIsOn = false;
+                bathStepString = bathStepString + " Cyrk. zakonczona.\n";
+                Serial.println("BathStep " + String(bathStep)); 
                 bathStep ++;
             }
             break;
@@ -288,6 +290,8 @@ void contolLogic(){
             hotWaterTankSetTemperature = BATH_WATER_TEMPERATURE;
             setTemperature(heaterSetTemperature);
             bathTempStartTime = steady_clock::now();
+            bathStepString = bathStepString + " Zadano T= " + String(heaterSetTemperature) + " C\n";
+            Serial.println("BathStep " + String(bathStep)); 
             bathStep ++;
             break;
 
@@ -297,42 +301,23 @@ void contolLogic(){
                 hotWaterTankSetTemperature = NORMAL_HOT_WATER_TEMPERATURE;
                 setTemperature(heaterSetTemperature);
                 acualMode = NORMAL;
+                bathStepString = bathStepString + " Tryb kąpieli zakończony.\n";
                 Serial.println("Bath mode ended");
             }
-            break;
+            break; 
         default:
             break;
         }
-        //turn on circulation valve only when temperature is ok
-        if (preBathCirculationTriggered && !preBathCirculationDone &&
-            (temperature1 > NORMAL_HOT_WATER_TEMPERATURE - HYSTERESIS) && (temperature2 > NORMAL_HOT_WATER_TEMPERATURE- HYSTERESIS)) {
-            pumpCirculationIsOn = true;
-            circtulationStartTime = steady_clock::now();
-            triggerCirculation = false;
-        })
-        //end circulation after given time
-        if(preBathCirculationTriggered && duration_cast<minutes>(now - circtulationStartTime).count() >= 15) {
-            
-        }
-            pumpCirculationIsOn = false;
-            acualMode = NORMAL;
-
-        heaterSetTemperature = BATH_WATER_TEMPERATURE;
-        hotWaterTankSetTemperature = BATH_WATER_TEMPERATURE;
-        setTemperature(heaterSetTemperature);
         break;
     default:
         break;
     }    
 
-    //control pumpHotWaterIsOn separately from other
-    //
-
-
-    if(hotWaterHeatingActive && temperature2 > hotWaterTankSetTemperature) {
+    //control pumpHotWaterIsOn separately from rest of logic
+    if(hotWaterHeatingActive && temperature2 > hotWaterTankSetTemperature && !pumpHotWaterIsOn){ 
         pumpHotWaterIsOn = true;
         Serial.println("Started pumpHotWaterIsOn");  
-    } elsif (!hotWaterHeatingActive || temperature2 < hotWaterTankSetTemperature - HYSTERESIS){
+    } else if ((!hotWaterHeatingActive || temperature2 < hotWaterTankSetTemperature - HYSTERESIS) && pumpHotWaterIsOn) {
         pumpHotWaterIsOn = false;
         Serial.println("Stopped pumpHotWaterIsOn");  
     }
@@ -344,7 +329,6 @@ void contolLogic(){
 
 // helper function for time handling
 void timeHelper() {
- /* Elapsed time handling*/
     auto now = steady_clock::now();
     //100ms
     if (duration_cast<milliseconds>(now - helperTime100ms).count() >= 100) {
@@ -390,7 +374,7 @@ void setTemperature(float temp) {
         }
         GP8413.setDACOutVoltage(setting_vol, 0);  
 
-        Serial.println("temperature was set to:  %0.2f " + String(temp));   
+        Serial.println("temperature was set to:  " + String(temp));   
     }
     
 }
--- src/screenHelper.cpp
+++ src/screenHelper.cpp
@@ -13,17 +13,21 @@ static void showMenu(bool refreshTick){
    
 }
 
-static void showSensorStatus(float temperature1, float temperature2, bool pumpHotWaterIsOn, bool pumpCirculationIsOn, bool pumpFloorHeatingIsOn, bool refreshTick){
+static void showSensorStatus(float heaterSetTemperature, float temperature1, float temperature2, bool pumpHotWaterIsOn, bool pumpCirculationIsOn, bool pumpFloorHeatingIsOn, bool refreshTick, int acualMode, int bathStep){
     if (refreshTick) {
         M5StamPLC.Display.clear(TFT_BLACK);
         M5StamPLC.Display.setCursor(0, 10);
         M5StamPLC.Display.setTextColor(TFT_WHITE);
         M5StamPLC.Display.setTextSize(2);   
+        M5StamPLC.Display.printf(" T. zadana:%.2fC\n", heaterSetTemperature);
         M5StamPLC.Display.printf(" T. zbiornika:%.2fC\n", temperature1);
         M5StamPLC.Display.printf(" T. sprzegla:%.2fC\n", temperature2);
         M5StamPLC.Display.printf(" Pompa CWU: %s\n", pumpHotWaterIsOn? "ON":"OFF");
         M5StamPLC.Display.printf(" Pompa cyrk.: %s\n", pumpCirculationIsOn? "ON":"OFF");
         M5StamPLC.Display.printf(" Pompa CO: %s\n", pumpFloorHeatingIsOn? "ON":"OFF");
+        M5StamPLC.Display.printf(" Tryb: %d\n", acualMode);
+        M5StamPLC.Display.printf(" Bath step: %d\n", bathStep);
+
     }
 }
 
@@ -33,18 +37,29 @@ static void showHeatingStarted(bool refreshTick){
         M5StamPLC.Display.setCursor(0, 10);
         M5StamPLC.Display.setTextColor(TFT_WHITE);
         M5StamPLC.Display.setTextSize(2);   
-        M5StamPLC.Display.println(" OGRZEWANIE");
-        M5StamPLC.Display.println(" ZOSTALO URUCHOMIONE");
+        M5StamPLC.Display.println(" TRYB KAPIELI");
+        M5StamPLC.Display.println(" WLACZONY");
     }
 }
-
-static void showHeatingStopped(bool refreshTick){
+static void showHeatingWithText(bool refreshTick, float heaterSetTemperature, float temperature1, String text){
     if (refreshTick) {
         M5StamPLC.Display.clear(TFT_BLACK);
         M5StamPLC.Display.setCursor(0, 10);
         M5StamPLC.Display.setTextColor(TFT_WHITE);
         M5StamPLC.Display.setTextSize(2);   
-        M5StamPLC.Display.println(" OGRZEWANIE");
-        M5StamPLC.Display.println(" ZOSTALO ZATRZYMANE");
+        M5StamPLC.Display.println(" TRYB KAPIELI");
+        M5StamPLC.Display.printf(" T. zadana:%.2fC\n", heaterSetTemperature);
+        M5StamPLC.Display.printf(" T. zbiornika:%.2fC\n", temperature1);
+        M5StamPLC.Display.println(text);
+    }
+}
+static void showNormalModeStarted(bool refreshTick){
+    if (refreshTick) {
+        M5StamPLC.Display.clear(TFT_BLACK);
+        M5StamPLC.Display.setCursor(0, 10);
+        M5StamPLC.Display.setTextColor(TFT_WHITE);
+        M5StamPLC.Display.setTextSize(2);   
+        M5StamPLC.Display.println(" TRYB NORMALNY");
+        M5StamPLC.Display.println(" WLACZANY");
     }
 }
